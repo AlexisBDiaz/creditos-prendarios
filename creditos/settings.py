862831a07c1c857767df4df38467ea7bef56db45
AlexisBD <171112@ids.upchiapas.edu.mx>
2020-09-07 15:09:46 -0500

    heroku bd

--- creditos/settings.py
+++ creditos/settings.py
@@ -72,21 +72,11 @@ WSGI_APPLICATION = 'creditos.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-} '''
-
-
-
-DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
-    
 }
 
 # Password validation
